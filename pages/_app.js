1a9884961776fad340e41758ba3fe09126289802
enisgultekinn <enisgltkn@gmail.com>
2023-06-28 10:39:18 +0300

    bootstrap icons added

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/reset.scss'
 import '../styles/main.scss'
+import 'bootstrap-icons/font/bootstrap-icons.css'
 
 export default function MyApp({ Component, pageProps: { ...pageProps } }) {
 	return <Component {...pageProps} />
